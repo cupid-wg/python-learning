a99bd967c569f81c4ae2f8f88226f40f8b99682c
cupid <weng.luo@gmail.com>
2016-01-16 15:58:06 +0800

    practise for python challege 2

---
+++ src/self/challenge/challenge2.py
@@ -0,0 +1,8 @@
+#http://www.pythonchallenge.com/pc/def/map.html
+#it's to practise python translate
+sfrom = "abcdefghijklmnopqrstuvwxyz"
+to = 'cdefghijklmnopqrstuvwxyzab'
+
+s = 'g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr\'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.'
+s = s.translate(str.maketrans(sfrom, to))
+print(s)
